f2171b7f845fd93d543db949022fb92fea57e69a
jacek <ola88mazurek@gmail.com>
2021-11-05 18:43:47 +0100

    application - calculate the value of a function

---
+++ CalculateTheValueOfTheFunctionApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace CalculateTheValueOfTheFunctionApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, y;
+
+            Console.WriteLine("Program oblicza wartość funkcji y = 19 * x");
+            Console.WriteLine("dla x zmieniającego się od 0 do 20.");
+
+            for (x = 0; x <= 20; x++)
+            {
+                y = 19 * x;
+                Console.WriteLine("x = " + x + '\t' + " y = " + y);
+            }
+        }
+    }
+}
